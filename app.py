2bc699f0225735784e89aee4ae49ec39dda570d3
ionCoj <ion07071@gmail.com>
2026-01-10 16:57:14 +0200

    Add requirements file for Render

--- app.py
+++ app.py
@@ -153,9 +153,10 @@ def CreateCookiesForUser(user):
     return redirect(url_for('playgame'))
     
 app = create_app()
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 if __name__ == "__main__":
-  login_manager = LoginManager()
-  login_manager.init_app(app)
-  login_manager.login_view = 'login'
+
   app.run(debug=True)
